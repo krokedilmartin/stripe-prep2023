f64483568f903e8d57054ac5d293b2febefdb870
Martin Lindahl <84283336+krokedilmartin@users.noreply.github.com>
2023-01-29 20:50:08 +0100

    status 200

--- backend/server.js
+++ backend/server.js
@@ -136,25 +136,25 @@ app.post('/stripe-complete', express.raw({type: 'application/json'}), (request, 
   console.log('martin event stripe-complete', event)
 
   // Handle the event
-  switch (event.type) {
-    case 'payment_intent.succeeded':
-      const paymentIntent = event.data.object;
-      console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`);
-      // Then define and call a method to handle the successful payment intent.
-      // handlePaymentIntentSucceeded(paymentIntent);
-      break;
-    case 'payment_method.attached':
-      const paymentMethod = event.data.object;
-      // Then define and call a method to handle the successful attachment of a PaymentMethod.
-      // handlePaymentMethodAttached(paymentMethod);
-      break;
-    default:
-      // Unexpected event type
-      console.log(`Unhandled event type ${event.type}.`);
-  }
+  // switch (event.type) {
+  //   case 'payment_intent.succeeded':
+  //     const paymentIntent = event.data.object;
+  //     console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`);
+  //     // Then define and call a method to handle the successful payment intent.
+  //     // handlePaymentIntentSucceeded(paymentIntent);
+  //     break;
+  //   case 'payment_method.attached':
+  //     const paymentMethod = event.data.object;
+  //     // Then define and call a method to handle the successful attachment of a PaymentMethod.
+  //     // handlePaymentMethodAttached(paymentMethod);
+  //     break;
+  //   default:
+  //     // Unexpected event type
+  //     console.log(`Unhandled event type ${event.type}.`);
+  // }
 
   // Return a 200 response to acknowledge receipt of the event
-  response.send();
+  response.status(200).send();
 });
 
 
