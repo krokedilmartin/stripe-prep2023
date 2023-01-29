90c4f5e230f9ca509095c3bf9f62c3e9162eacd5
Martin Lindahl <84283336+krokedilmartin@users.noreply.github.com>
2023-01-29 21:18:04 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -138,7 +138,7 @@ app.post('/stripe-complete', express.raw({type: 'application/json'}), (request, 
   // Handle the event
   switch (event.type) {
     case 'checkout.session.completed':
-      console.log('skapa wc order', event.data)
+      console.log('skapa wc order', event.data.object)
       break;
     default:
       // Unexpected event type
